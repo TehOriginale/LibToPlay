d9c485d915263fda92e479f0e74955e3aee7b82e
TehOriginale <78594680+TehOriginale@users.noreply.github.com>
2021-02-19 12:29:36 +0200

    Extra

---
+++ Ugnius CodeAcademy/c++/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    int n;
+
+    fd = ifstream.open("Duomenys.txt");
+    fr = ofstream.open("Rezultatai.txt");
+
+    fd >> n;
+
+    cout>>n;
+
+
+
+
+
+
+
+
+}
