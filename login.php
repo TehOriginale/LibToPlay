b2d7d75a227450e235e23b308f3fbb31ea96f363
TehOriginale <78594680+TehOriginale@users.noreply.github.com>
2021-02-19 10:55:49 +0200

    File Change

--- login.php
+++ login.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title> 
-    This is the title page askjdhaks as a
-</title>
+    <title>Document</title>
 </head>
 <body>
-    <script src="js/scripts.js"></script>
-
+    
 </body>
 </html>
