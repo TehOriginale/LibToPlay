a59618a0ff19d566889fecf7bf7c91368d18e06c
TehOriginale <78594680+TehOriginale@users.noreply.github.com>
2021-02-19 11:45:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,22 @@
   <h2>Button Tags</h2>
   <a href="Cia reikia login page linko" class="btn btn-info" role="button">Login</a>
   </div>
-  
+
+  <form>
+  <div class="filter-group">
+    <label for="ImputGame1">Game#1</label>
+    <input type="FirstGame" class="form-control" id="ImputGame1" placeholder="Declare your first game">
+   
+  </div>
+  <div class="filter-group">
+    <label for="ImputGame2">Game#2</label>
+    <input type="Secondgame" class="form-control" id="ImputGame2" placeholder="Declare your second game">
+  </div>
+  <div class="filter-group">
+    <label for="ImputGame3">Game#3</label>
+    <input type="ThirdGame" class="form-control" id="ImputGame3" placeholder="Declare your third game">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </body>
 </html>
