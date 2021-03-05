dc6f9029b42cd6f3e96e8edc4025a0c0211b40f6
PavlikStudies <paulius.klimaitis@ktu.edu>
2021-03-05 14:54:13 +0200

    Index Pakeitimai

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+ 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,15 +11,28 @@
     <link href="css/index.css" rel ="stylesheet">
 </head>
 <body>
-    <div>
-        <img src  = "Thumbnail link" class  = "img-rounded" alt = "Logotipas">
+  <div>
+  <a class="LogoItem" href = "/index.php"> <img src ="content/test_logo.png" alt = "Logotipias" height = "26" width = "26">
+</a>
 </div>
-<div class="container">
-  <h2>Button Tags</h2>
-  <a href="Cia reikia login page linko" class="btn btn-info" role="button">Login</a>
+<div class="dropdown">
+
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="Menu Button">
+    <div>
+    <a class="Login" href="/login.php">Login</a>
+</div>
+<div>
+    <a class="dropdown-item" href="/signup.php">Sign up</a>
+    </div>
+    <div>
+    <a class="dropdown-item" href="/gameinfo.php">Random game of the day</a>
+    </div>
   </div>
-  <main class = "Index_main" > 
-    
+</div>
+
   <form>
   <div class="filter-group">
     <label for="ImputGame1">Game#1</label>
@@ -34,7 +49,13 @@
   </div>
   
 </form>
-<button type="submit" class="btn btn-primary">Submit</button>
+<div id ="SubButton">
+<button type="submit" class="btn">Submit</button>
+</div>
   </main>
+  <scrip type = "text/javascript" src= bootstrap/js/bootstrap.bundle.js> </script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
