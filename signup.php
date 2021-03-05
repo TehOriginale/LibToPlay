471ef4cbacc8936b1484573a87adf503805d5fa4
TehOriginale <78594680+TehOriginale@users.noreply.github.com>
2021-03-05 13:34:52 +0200

    update signup

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/login.css" rel="stylesheet">
+    <link href="css/signup.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300&display=swap" rel="stylesheet">
 </head> 
@@ -15,7 +15,7 @@
     <main class="form-signin">
         <form>
             <a href="index.php"><img class="mb-4"src="content/test_logo.png" alt="" width="140" height="200"></a>
-            <h1>Login</h1>
+            <h1>Sign up</h1>
             <label for="inputEmail" class="visually-hidden">
                 Email adress
             </label>
@@ -24,15 +24,16 @@
                 Password
             </label>
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+            <label for="confirmPassword" class="visually-hidden"> Confirm Password </label>
+            <input type="confirm" id="confirmPassword" class="form-control" placeholder="Confirm Password" required>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
 
             
         </form>
         <div class="box-signin">
-        New to LibToPlay? 
+        Already have an account? 
         <div class="divider"></div>
-        <a id="signin" href="signup.php">Create an account</a>
+        <a id="signin" href="login.php">Login</a>
         </div>
         
     </main>
