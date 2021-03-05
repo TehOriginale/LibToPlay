dbe7abe42128d47779b12a1f68ab9b2ce2c4ad15
TehOriginale <78594680+TehOriginale@users.noreply.github.com>
2021-03-05 12:23:45 +0200

    login update

--- login.php
+++ login.php
@@ -6,21 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
 </head> 
+
 <body class="text-center">
-    <main>
+    <main class="form-signin">
         <form>
-            <label for="inputEmail">
+            <img >
+            <h1>Login</h1>
+            <label for="inputEmail" class="visually-hidden">
                 Email adress
             </label>
-            <input>
-
-            <label>
-                Passwordd
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
+            <label for="inputPassword" class="visually-hidden">
+                Password
             </label>
-            <input> 
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
 
+            
         </form>
+        <div class="box-signin">
+        New to LibToPlay? 
+        <div class="divider"></div>
+        <a>Create an account</a>
+        </div>
+        
     </main>
 </body>
 </html>
