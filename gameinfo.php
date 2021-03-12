95ac5587178877e75eac94dde55c9d3a038a55ab
PavlikStudies <paulius.klimaitis@ktu.edu>
2021-03-12 12:05:50 +0200

    Gameinfo

--- gameinfo.php
+++ gameinfo.php
@@ -6,8 +6,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/gameinfo.css" rel ="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
+<div class = "gamePhoto" >
+<figure class="figure">
+<a style="float:right" class="LogoItem" href = "/RDR2Art"> <img src ="content/RDR2Art.jpg" alt = "ZaidimoArt" height = "600" width = "260">
+<figcaption class="figure-caption">Perfecto game 5 youuu bby!</figcaption>
+</figure>
+</a>
+</div>
+<div id = tekstas>
+<h1>
+Game name pull'as
+</h1>
+<h3>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Nunc mi dolor, auctor vel dui nec, laoreet aliquam odio. 
+Quisque sit amet ex ut est sagittis consectetur. 
+Ut dictum orci vitae lacus tincidunt dictum. Proin vestibulum augue arcu, 
+in porttitor urna dapibus sed. Fusce venenatis mi non metus rutrum volutpat.
+Phasellus eget pharetra velit. Praesent ultricies lobortis est, eu sodales elit placerat ac.
+Curabitur non rutrum sapien. Proin quis lacinia tellus. Nulla facilisi. Lorem ipsum dolor
+sit amet, consectetur adipiscing elit. Nunc convallis et quam eget imperdiet.
+Nam in erat condimentum, consectetur lacus eu, sodales augue. Fusce tincidunt
+iaculis erat at consectetur. Integer accumsan tellus sed libero consequat faucibus. 
+Cras dignissim diam dignissim suscipit lacinia. In eget neque et quam ullamcorper scelerisque a pretium nunc.
+Vestibulum feugiat tempor ligula, nec auctor leo mattis sit amet. Lorem ipsum dolor sit amet,
+consectetur adipiscing elit. Duis in tempus mi, in lacinia ligula. Quisque facilisis eros nulla,
+ultricies cursus libero aliquet at. Mauris eget ligula sit amet velit dapibus eleifend.
+Proin nibh purus, sagittis ut laoreet hendrerit, porta et odio. Donec fringilla velit
+sed sapien euismod, sed tincidunt libero ultricies. Nullam faucibus tellus vel ullamcorper mollis. 
+Pellentesque condimentum hendrerit nisi, non sodales nisi. Donec facilisis metus vel magna porta hendrerit. 
+Morbi tempor hendrerit pretium. Maecenas eget euismod neque. Vivamus ac ultrices mi. 
+Sed non vehicula tellus. Mauris porttitor lectus eu accumsan finibus.
+Integer pulvinar nec augue elementum feugiat. Vivamus id viverra lorem, 
+quis feugiat augue. Nulla pretium, quam vel molestie iaculis, ligula quam fringilla turpis, 
+eu facilisis dolor ex tincidunt libero. Duis vel aliquam est. Cras quam metus, egestas ut lobortis 
+eget, ultricies in lectus. Quisque vehicula laoreet leo, eget finibus mi suscipit ut. Duis et magna ullamcorper, 
+malesuada lectus sed, venenatis justo.
+Mauris sollicitudin maximus nisl, eu iaculis massa hendrerit sed. Vestibulum feugiat, ante ut porta sagittis, 
+nisi elit malesuada augue, sit amet elementum felis ante a mi. Aenean nec enim ante. 
+Curabitur tempor nisi nec lacinia congue. Aliquam consequat placerat fringilla. Class aptent taciti sociosqu 
+ad litora torquent per conubia nostra, per inceptos himenaeos. Donec sit amet mollis urna. Nam 
+posuere scelerisque massa. Morbi tempor consequat eros efficitur blandit. Ut sed diam a nisl iaculis laoreet. 
+Duis feugiat, nisi in blandit tempor, felis ligula rhoncus elit, at viverra augue urna finibus nisi. 
+Morbi vestibulum id risus et dignissim. Vivamus tempor bibendum rhoncus. Morbi sit amet massa sodales, 
+faucibus tortor vitae, convallis nisl. Maecenas suscipit justo laoreet odio hendrerit sagittis.
+</h3>
+
+</div>
+<div class = Lentele>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">PRating</th>
+      <th scope="col">Grating</th>
+      <th scope="col">WhereToBuy</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>SMTH/5</td>
+      <td>SMTH-1/5</td>
+      <td> Amazon<a class="btn btn-success" href="http://www.amazon.com/" role="button" alt = Badabish>BuyNow</a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>PRating</td>
+      <td>GRating</td>
+      <td> Steam<a class="btn btn-success" href="https://store.steampowered.com/" role="button" alt = BadabishSteam>BuyNow!!</a></td>
+    </tr>
+  </tbody>
+</table>
+</div>
     
 </body>
 </html>
